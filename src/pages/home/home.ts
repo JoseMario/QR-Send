a37622bff95981768e0c7e4cf04d3e76d9175c84
MarioRguez <josemario.rodriguez.sanchez@bbva.com>
2018-09-09 03:29:50 -0500

    Inicial lectura de código QR

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { BarcodeScanner ,BarcodeScannerOptions } from '@ionic-native/barcode-scanner';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+	layoutType : {};
+	scanData : {};
+	options :BarcodeScannerOptions;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public barcodeScanner: BarcodeScanner) {
+  	this.layoutType = {
+  		"TD" : "Tarjeta de débito",
+  		"TC" : "Tarjeta de crédito",
+  		"CL" : "CLABE interbancaria",
+  		"00012" : "BBVA Bancomer",
+  		"MX" : "México"
+  	};
   }
+
+  scan(){
+    this.options = {
+      prompt : "Primero una disculpa, pero scannea tu QR!!"
+    }
+    this.barcodeScanner.scan(this.options).then((barcodeData) => {
+      this.scanData = JSON.parse(barcodeData["text"]);
+    }, (err) => {
+        console.log("Error occured : " + err);
+    });         
+	}    
 
 }
